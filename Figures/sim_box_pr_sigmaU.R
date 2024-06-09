71b40ae3ba23cb464ba3ac11701447b90b346113
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-06-08 21:42:04 -0400

    Commit sim_box_pre_sigmaU (.R, .png)

--- Figures/sim_box_pr_sigmaU.R
+++ Figures/sim_box_pr_sigmaU.R
@@ -11,6 +11,36 @@ res = do.call(what = rbind,
                             FUN = read.csv)
 )
 
+# Create summary of results
+res |> 
+  dplyr::group_by(N, sigmaU) |> 
+  dplyr::summarize(avg_fpr = mean(fpr), 
+                   bias_beta0_gs = mean(beta0_gs - beta0), 
+                   ese_beta0_gs = sd(beta0_gs), 
+                   bias_beta1_gs = mean(beta1_gs - beta1), 
+                   ese_beta1_gs = sd(beta1_gs), 
+                   bias_beta0_n = mean(beta0_n - beta0), 
+                   ese_beta0_n = sd(beta0_n), 
+                   bias_beta1_n = mean(beta1_n - beta1), 
+                   ese_beta1_n = sd(beta1_n), 
+                   bias_beta0_cc = mean(beta0_cc - beta0), 
+                   ese_beta0_cc = sd(beta0_cc), 
+                   bias_beta1_cc = mean(beta1_cc - beta1), 
+                   ese_beta1_cc = sd(beta1_cc), 
+                   re_beta1_cc = var(beta1_gs) / var(beta1_cc),
+                   bias_beta0_smle = mean(beta0_smle - beta0), 
+                   ese_beta0_smle = sd(beta0_smle), 
+                   ase_beta0_smle = mean(se_beta0_smle), 
+                   bias_beta1_smle = mean(beta1_smle - beta1), 
+                   ese_beta1_smle = sd(beta1_smle), 
+                   ase_beta1_smle = mean(se_beta1_smle), 
+                   cp_beta1_smle = mean((beta1_smle - 1.96 * se_beta1_smle) <= beta1 & 
+                                          beta1 <= (beta1_smle + 1.96 * se_beta1_smle)), 
+                   re_beta1_smle = var(beta1_gs) / var(beta1_smle)
+  ) |> 
+  write.csv("~/Documents/food_access_misclassification/Simulations/sims_vary_sigmaU_binX_summary.csv", 
+            row.names = FALSE)
+
 # Define color palette 
 # slide_colors = c("#C3CFFA", "#C9B0B0", "#DD6D53", "#60CAD6", "#F0D290", "#fac3cf") 
 slide_colors = c("#F2958D", "#ADA264", "#EB5F3F", "#3A5724", "#FAB825", "#E67B33") 
@@ -18,7 +48,8 @@ slide_colors = c("#F2958D", "#ADA264", "#EB5F3F", "#3A5724", "#FAB825", "#E67B33
 # Create boxplot of estimated 
 est_box = res |> 
   dplyr::select(-dplyr::starts_with("se_"), 
-                -dplyr::contains("beta0")) |> 
+                -dplyr::contains("beta0"), 
+                -ppv_smle) |> 
   tidyr::gather(key = "Method", value = "est_beta1", -c(1:8)) |> 
   dplyr::mutate(Method = factor(x = Method, 
                                 levels = c("beta1_gs", "beta1_smle", "beta1_cc", "beta1_n"), 
