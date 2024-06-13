f122b3f4620748c4cb1faf9c22c5f9d563532023
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-06-13 09:34:04 -0400

    Commit sims_vary_q (.R, .csv)

--- Simulations/sims_vary_q_binX_from_contX.R
+++ Simulations/sims_vary_q_binX_from_contX.R
@@ -21,26 +21,42 @@ num_reps = 1000
 
 # Set parameters that won't be varied in the loop
 ## These values will be set as the defaults in the sim_data() function for convenience
-fix_beta0 = -2.2456410 ## outcome model intercept (leads to ~ 11% prevalence)
+fix_beta0 = -2.2456410 ## outcome model intercept (leads to ~ 7% prevalence)
 fix_beta1 = 0.1549768 ## log prevalence ratio for X on Y
+fix_muU = -0.7 ## error mean
+fix_sigmaU = 0.8 ## error standard deviation
+fix_access = 1 ## mile threshold to discretize X into access/no access
 
 # ---------------------------------------------------------------------------------
 # Function to simulate data (arguments defined as follows) ------------------------
 ## N = number of neighborhoods (sample size)
 ## beta0 = model intercept
 ## beta1 = log prevalence ratio 
+## muU = mean of the measurement error distribution
+## sigmaU = standard deviation of the measurement error distribution
+## access = threshold (in miles) used to discretize proximity into access/no access
 # ---------------------------------------------------------------------------------
-sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, beta2 = fix_beta2) {
-  ## Simulate straight-line access
-  binXstar = rbinom(n = N, 
-                    size = 1, 
-                    prob = 0.5)
+sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, muU = fix_muU, sigmaU = fix_sigmaU, access = fix_access) {
+  ## Simulate true (map-based) proximity to grocery store
+  X = rgamma(n = N,
+             shape = 1,
+             scale = 2.5) 
   
-  ## Simulate map-based access
-  binX = binXstar
-  binX[binXstar == 1] = rbinom(n = sum(binXstar == 1), 
-                               size = 1, 
-                               prob = 0.6)
+  ## Construct discretived true (map-based) access
+  binX = as.numeric(X <= access)
+  
+  ## Simulate random errors
+  U = truncnorm::rtruncnorm(n = N, 
+                            a = -Inf, 
+                            b = 0, 
+                            mean = muU, 
+                            sd = sigmaU)
+  
+  ## Construct error-prone (straight-line) proximity to grocery store
+  Xstar = X + U ### assuming additive measurement error model
+  
+  ## Construct discretived error-prone (straight-line) access
+  binXstar = as.numeric(Xstar <= access)
   
   ## Simulate population
   P = rpois(n = N, 
@@ -52,7 +68,7 @@ sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, beta2 = fix_beta2) 
                 lambda = P * lambda)
   
   ## Create dataset
-  dat = data.frame(id = 1:N, binX, binXstar, P, Cases)
+  dat = data.frame(id = 1:N, X, Xstar, binX, binXstar, P, Cases)
   
   # Return dataset
   return(dat)
@@ -69,12 +85,12 @@ for (N in c(390, 2200)) {
     # Create dataframe to save results for setting
     sett_res = data.frame(
       sim = paste(sim_seed, 1:num_reps, sep = "-"), 
-      N, beta0 = fix_beta0, beta1 = fix_beta1, ## simulation setting
-      q = q, avg_prev = NA, fpr = NA, ppv = NA, ## simulation setting
+      N, beta0 = fix_beta0, beta1 = fix_beta1, muU = fix_muU, ## simulation setting
+      sigmaU = fix_sigmaU, q = q, avg_prev = NA, fpr = NA, ## simulation setting
       beta0_gs = NA, se_beta0_gs = NA, beta1_gs = NA, se_beta1_gs = NA, ## gold standard analysis
       beta0_n = NA, se_beta0_n = NA, beta1_n = NA, se_beta1_n = NA, ## naive analysis
       beta0_cc = NA, se_beta0_cc = NA, beta1_cc = NA, se_beta1_cc = NA, ## complete case analysis
-      beta0_smle = NA, se_beta0_smle = NA, beta1_smle = NA, se_beta1_smle = NA, ppv_smle = NA ## imputation analysis
+      beta0_smle = NA, se_beta0_smle = NA, beta1_smle = NA, se_beta1_smle = NA ## imputation analysis
     )
     
     # Loop over replicates 
@@ -89,15 +105,6 @@ for (N in c(390, 2200)) {
       sett_res$fpr[r] = sum(dat$binX == 0 & dat$binXstar == 1) /
         (sum(dat$binX == 0 & dat$binXstar == 1) + 
            sum(dat$binX == 0 & dat$binXstar == 0))
-      
-      # Save false positive rate
-      sett_res$fpr[r] = sum(dat$binX == 0 & dat$binXstar == 1) /
-        (sum(dat$binX == 0 & dat$binXstar == 1) + 
-           sum(dat$binX == 0 & dat$binXstar == 0))
-      
-      # Save positive predictive value
-      sett_res$ppv[r] = sum(dat$binX == 1 & dat$binXstar == 1) /
-        sum(dat$binXstar == 1)
       
       # Fit the gold standard model
       fit_gs = glm(formula = Cases ~ binX, 
@@ -139,7 +146,6 @@ for (N in c(390, 2200)) {
                             data = dat)
       sett_res[r, c("beta0_smle", "beta1_smle")] = fit_smle$coeff$coeff ## estimated log prevalence ratio
       sett_res[r, c("se_beta0_smle", "se_beta1_smle")] = fit_smle$coeff$se ## and its standard error
-      sett_res[r, "ppv_smle"] = fit_smle$ppv
       
       # Save results
       write.csv(x = sett_res,
@@ -151,5 +157,5 @@ for (N in c(390, 2200)) {
 }
 
 # Timing from tictoc:
-# Sims with N = 390: 156.96 sec elapsed
-# Sims with N = 2200: 518.087 sec elapsed
+## Sims with N = 390: 154.936 sec elapsed
+## Sims with N = 2200: 528.144 sec elapsed
